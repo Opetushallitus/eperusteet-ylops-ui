0722b93dc3ebd43e633b878c744c89ebfcdfdbfd
baabelfish <niko.junkala@gofore.com>
2019-01-03 12:44:07 +0200

    Lisätty oikea nimeämiskäytäntö

--- scripts/buildapi.sh
+++ scripts/buildapi.sh
@@ -4,16 +4,16 @@
 if [[ -z "${YLOPS_SERVICE_DIR}" ]]; then
   echo -ne "\x1b[1m\$YLOPS_SERVICE_DIR ei ole asetettu.\n" 
   echo -ne "export YLOPS_SERVICE_DIR=\${HOME}/eperusteet-ylops/eperusteet-ylops-service\n" 
-  echo -e "\x1b[0m"
+  echo -ne "\x1b[0m"
   exit 1
 fi
 
 mkdir -p src/generated
 cd src/generated
 curdir=$(pwd)
-specfile="$YLOPS_SERVICE_DIR/target/openapi/ylops.spec.json"
 
+specfile="$YLOPS_SERVICE_DIR/target/openapi/ylops.spec.json"
 cd ${YLOPS_SERVICE_DIR} \
   && mvn compile \
   && cd $curdir \
-  && openapi-generator generate -i "${specfile}" -g typescript-axios
+  && openapi-generator generate -c ../../generator.config.json -i "${specfile}" -g typescript-axios
