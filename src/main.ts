48ef09e2742ea1d133bed2dc7e44db176370b6db
baabelfish <niko.junkala@gofore.com>
2019-01-04 18:53:30 +0200

    Lisätty tyypitetty wrapperi Vuexille
    - Lisätty esimerkki sen käytöstä
    - views -> routes
    
    Ei käytetä juurikomponenttiin injektoitua instanssia, koska sitä ei voi
    tyypittää. Tyypitys on mahdollista päätason storelle, mutta ei sen
    moduuleille periyttämällä Vuen.

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,11 @@ import '@/config/styles';
 
 import App from './App.vue';
 import router from './router';
-import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount('#app');
--- src/routes/home/script.ts
+++ src/routes/home/script.ts
@@ -1,9 +1,14 @@
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/hello/component.vue';
+import { Kayttajat } from '@/stores/kayttaja';
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private get kayttaja() {
+    return Kayttajat.tiedot;
+  }
+}
