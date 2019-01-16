c18424d1b75e715e933d683b53286e2abfcf2ed6
Sauli Anto <sauli.anto@gmail.com>
2019-01-16 13:48:31 +0200

    Korjattu buildapi.sh

--- scripts/buildapi.sh
+++ scripts/buildapi.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # Generoi SDK:n apikuvauksen perusteella.
 
-if [[ -z "${YLOPS_SERVICE_DIR}" ]]; then
-  echo -ne "\x1b[1m\$YLOPS_SERVICE_DIR ei ole asetettu.\n" 
-  echo -ne "export YLOPS_SERVICE_DIR=\${HOME}/eperusteet-ylops/eperusteet-ylops-service\n" 
-  echo -ne "\x1b[0m"
+if [[ -z "${YLOPS_SERVICE_DIR}" ]]
+then
+  printf "\x1b[1m\$YLOPS_SERVICE_DIR ei ole asetettu.\n"
+  printf "export YLOPS_SERVICE_DIR=\${HOME}/eperusteet-ylops/eperusteet-ylops-service\n"
+  printf "\x1b[0m"
   exit 1
 fi
 
