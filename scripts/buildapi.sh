be6d081c5962e5288c2258cfd687e1abe636a37c
baabelfish <niko.junkala@gofore.com>
2019-02-01 13:04:48 +0200

    CC korjauksia

--- scripts/buildapi.sh
+++ scripts/buildapi.sh
@@ -5,17 +5,17 @@ if [[ -z $YLOPS_SERVICE_DIR ]]
 then
   printf "\x1b[1mYLOPS_SERVICE_DIR\x1b[0m is not set.\n"
   printf "For example, call export \x1b[1mYLOPS_SERVICE_DIR="
-  printf $HOME
+  printf "%s" "$HOME"
   printf "/eperusteet-ylops/eperusteet-ylops-service\x1b[0m\n"
   exit 1
 fi
 
 mkdir -p src/generated
-cd src/generated
+cd src/generated || exit 1
 gendir=$(pwd)
 
 specfile="$YLOPS_SERVICE_DIR/target/openapi/ylops.spec.json"
-cd $YLOPS_SERVICE_DIR \
+cd "$YLOPS_SERVICE_DIR" \
   && mvn clean compile \
-  && cd $gendir \
-  && npx openapi-generator generate -c ../../generator.config.json -i $specfile -g typescript-axios
+  && cd "$gendir" \
+  && npx openapi-generator generate -c ../../generator.config.json -i "$specfile" -g typescript-axios
