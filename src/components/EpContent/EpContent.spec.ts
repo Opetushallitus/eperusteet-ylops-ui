8953d070ca00b340fe678051d728d53a65662827
NJ <niko.junkala@gofore.com>
2019-02-28 17:37:15 +0200

    Korjattu testejä

--- src/components/EpContent/EpContent.spec.ts
+++ src/components/EpContent/EpContent.spec.ts
@@ -2,25 +2,37 @@ import { mount } from '@vue/test-utils';
 import EpContent from './EpContent.vue';
 
 describe('EpContent component', () => {
-    it('Renders given string content', async () => {
-        const wrapper = mount(EpContent, {
-            propsData: { value: '<p>Non editable</p>', isEditable: false },
-        });
-
-        expect(wrapper.html()).toContain('Non editable');
+  it('Renders given string content', async () => {
+    const wrapper = mount(EpContent, {
+      propsData: {
+        value: '<p>Non editable</p>',
+        isEditable: false,
+        lokalisoitu: false,
+      },
     });
 
-    it('Renders given object content', () => {
-        const wrapper = mount(EpContent, {
-            propsData: { value: { fi: 'TestMsg' } },
-        });
+    expect(wrapper.html()).toContain('Non editable');
+  });
 
-        expect(wrapper.html()).toContain('TestMsg');
+  it('Renders given object content', () => {
+    const wrapper = mount(EpContent, {
+      propsData: {
+        value: {
+          fi: 'TestMsg',
+        },
+      },
     });
 
-    it('does not render anything with null value', () => {
-        const wrapper = mount(EpContent, {});
-        expect(wrapper.text()).toEqual('');
+    expect(wrapper.html()).toContain('TestMsg');
+  });
+
+  it('does not render anything with null value', () => {
+    const wrapper = mount(EpContent, {
+      propsData: {
+        value: null,
+      },
     });
+    expect(wrapper.text()).toEqual('');
+  });
 
 });
