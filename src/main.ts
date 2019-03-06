75c29fefdeeb437105e065c917ee89c1decaf5bd
jspeha <pekka.harjamaki@gofore.com>
2019-03-06 12:42:02 +0200

    Korjattu lisää travis ongelmia

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,13 @@ import '@/config/styles';
 import { rootConfig } from '@/mainvue';
 import '@/registerServiceWorker';
 import { createLogger } from '@/stores/logger';
-import devtools from '@vue/devtools';
 import Vue from 'vue';
 
 Vue.use(devtools);
 
 const logger = createLogger('Main');
-const inDev = process.env.NODE_ENV === 'development';
-
-if (inDev) {
-  devtools.connect();
-}
 
 Vue.config.productionTip = false;
-Vue.config.devtools = inDev;
 Vue.config.performance = inDev;
 
 async function main() {
