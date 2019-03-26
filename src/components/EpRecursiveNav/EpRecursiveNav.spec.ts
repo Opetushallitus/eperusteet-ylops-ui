3b957a3b5e25ff162a7a3bdeb9523ddbbaa6f666
jspeha <pekka.harjamaki@gofore.com>
2019-03-26 11:19:05 +0200

    Lisätty tekstikappaleet + parempi testikattavuus (#65)
    * Lisätty tekstikappaleet + parempi testikattavuus
    
    * Valikko reagoi nyt kielen vaihtoon
    
    * Siivottu rakenteita

--- src/components/EpRecursiveNav/EpRecursiveNav.spec.ts
+++ src/components/EpRecursiveNav/EpRecursiveNav.spec.ts
@@ -34,20 +34,59 @@ const testData = [
         },
       },
     ],
-  },
+  }, {
+    item: {
+      name: 'Alivalikko',
+    },
+    children: [
+      {
+        item: {
+          name: 'Toinen taso',
+        },
+        children: [
+          {
+            item: {
+              name: 'testi',
+            }
+          },
+        ],
+      },
+    ],
+  }
 ];
 
 describe('EpRecursiveNav component', () => {
-  it('Renders header and content', () => {
+  it('navigates the menu structure properly', () => {
     const wrapper = mount(EpRecursiveNav, {
       propsData: {
         value: testData,
       },
       scopedSlots: {
-        default: '<div>{{props.item.name}}</div>',
+        previousLink: '<div id="prevlink" @click="props.navigate()">{{props.item.name}}</div>',
+        default: '<div v-if="!props.isSubmenu">{{props.item.name}}</div>'
+          + '<div v-else @click="props.navigate(props.item,props.children)" id="sublink">{{props.item.name}}</div>',
       },
     });
 
     expect(wrapper.html()).toContain('Dokumentit');
+    expect(wrapper.html()).not.toContain('prevlink');
+
+    // Trigger 'vaihdaValikkoa' method
+    wrapper.find('#sublink').trigger('click');
+    expect(wrapper.html()).toContain('Toinen taso');
+    expect(wrapper.html()).toContain('prevlink');
+
+    wrapper.find('#sublink').trigger('click');
+    expect(wrapper.html()).toContain('testi');
+    expect(wrapper.html()).toContain('prevlink');
+    expect(wrapper.html()).not.toContain('Alivalikko');
+
+    // Trigger 'palaaTakaisin' method twice. We should end at starting point
+    wrapper.find('#prevlink').trigger('click');
+    expect(wrapper.html()).toContain('Alivalikko');
+
+    wrapper.find('#prevlink').trigger('click');
+    expect(wrapper.html()).toContain('Dokumentit');
+    expect(wrapper.html()).not.toContain('prevlink');
   });
 });
