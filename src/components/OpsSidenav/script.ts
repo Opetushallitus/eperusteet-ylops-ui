a013afa5e9de0b9ab5eee67b5c10ef70fe9d4be4
jspeha <pekka.harjamaki@gofore.com>
2019-03-28 11:07:30 +0200

    opssidenav codeclimate korjaus

--- src/components/OpsSidenav/script.ts
+++ src/components/OpsSidenav/script.ts
@@ -111,21 +111,17 @@ export default class OpsSidenav extends Vue {
     }
   }
 
-  private taydennaMenuData(menuData: any, locale: string) {
+  private taydennaMenuData(menuData: any, lang: string) {
     for (let menuItem of menuData) {
       if (menuItem.route) {
-        if (menuItem.route.params) {
-          menuItem.route.params.lang = locale;
-        }
-        else {
-          menuItem.route.params = {
-            lang: locale,
-          };
-        }
+        menuItem.route.params = {
+          ...menuItem.route.params,
+          lang,
+        };
       }
 
       if (menuItem.children) {
-        this.taydennaMenuData(menuItem.children, locale);
+        this.taydennaMenuData(menuItem.children, lang);
       }
     }
   }
@@ -205,13 +201,9 @@ export default class OpsSidenav extends Vue {
     }
 
     const locale = Kielet.getSisaltoKieli();
+    const i18key = (value.type === 'tekstikappale') ? 'nimetön-tekstikappale' : 'nimetön-oppiaine';
 
-    if (value.type === 'tekstikappale') {
-      return (value.name as any)[locale] || this.$t('nimetön-tekstikappale');
-    }
-    else {
-      return (value.name as any)[locale] || this.$t('nimetön-oppiaine');
-    }
+    return (value.name as any)[locale] || this.$t(i18key);
   }
 
   private get opsLapset() {
