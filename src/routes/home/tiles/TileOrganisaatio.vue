7907874af82378f62e459cf852a59f260b3e95b6
Sauli Anto <sauli.anto@gmail.com>
2019-04-01 14:27:44 +0300

    Aloitettu tekemään organisaatio työryhmää

--- src/routes/home/tiles/TileOrganisaatio.vue
+++ src/routes/home/tiles/TileOrganisaatio.vue
@@ -1,27 +1,34 @@
 <template lang="pug">
-base-tile(icon="tyoryhma", color="#82D4FF")
+base-tile(icon="tyoryhma", color="#82D4FF", :route="{ name: 'organisaatio' }")
   template(slot="header")
     span {{ $t('tile-organisaatio') }}
   template(slot="content")
     p {{ $t('tile-organisaatio-kuvaus') }}
     b-row.henkilot
-      b-col.henkilo(sm="6")
-        span.nimi Henkilö X
-      b-col.henkilo(sm="6")
-        span.nimi Henkilö Y
-      b-col.henkilo(sm="6")
-        span.nimi Henkilö Z
-    button.btn.btn-link {{ $t('nayta-lisaa') }}
+      b-col(sm="6")
+        ep-color-ball.mr-2
+        span Henkilö X
+      b-col(sm="6")
+        ep-color-ball.mr-2
+        span Henkilö Y
+      b-col(sm="6")
+        ep-color-ball.mr-2
+        span Henkilö Z
+    p.mt-3 {{ $t('nayta-lisaa') }}
 
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import BaseTile from './BaseTile.vue';
+import {
+  EpColorBall,
+} from '@/components';
 
 @Component({
   components: {
     BaseTile,
+    EpColorBall,
   },
 })
 export default class TileOrganisaatio extends Vue {
@@ -33,7 +40,5 @@ export default class TileOrganisaatio extends Vue {
 .henkilot {
   margin: 5px;
 
-  .henkilo {
-  }
 }
 </style>
---
+++ src/routes/organisaatio/RouteOrganisaatio.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+
+ep-main-view
+  template(slot="icon")
+    ep-icon.float-right(icon="tyoryhma", background-color="#82D4FF")
+
+  template(slot="header")
+    h1 {{ $t('organisaatio-tyoryhma') }}
+    p {{ $t('organisaatio-tyoryhma-kuvaus') }}
+
+</template>
+
+<script lang="ts">
+import { Vue, Component, Mixins } from 'vue-property-decorator';
+import _ from 'lodash';
+
+import EpRoute from '@/mixins/EpRoot';
+import { Kielet } from '@/stores/kieli';
+
+import {
+  EpAikaleima,
+  EpCollapse,
+  EpContent,
+  EpIcon,
+  EpKaanna,
+  EpMainView,
+  EpNavigation,
+  EpSearch,
+  EpSpinner,
+} from '@/components';
+import { Ulkopuoliset } from '@/api';
+
+@Component({
+  components: {
+    EpAikaleima,
+    EpCollapse,
+    EpContent,
+    EpIcon,
+    EpKaanna,
+    EpMainView,
+    EpNavigation,
+    EpSearch,
+    EpSpinner,
+  },
+})
+export default class RouteOrganisaatio extends Mixins(EpRoute) {
+  async init() {
+    // await this.update();
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
