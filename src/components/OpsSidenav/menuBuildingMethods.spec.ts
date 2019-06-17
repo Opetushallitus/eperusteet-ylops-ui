5e13e40d178af7a626674424a9924ce4d66199ef
jspeha <pekka.harjamaki@gofore.com>
2019-06-17 18:05:24 +0300

    Lisätty yksikkötestejä valikkorakenteen luontiin

--- src/components/OpsSidenav/menuBuildingMethods.spec.ts
+++ src/components/OpsSidenav/menuBuildingMethods.spec.ts
@@ -7,7 +7,7 @@ import {
 } from './menuBuildingMethods';
 
 describe('OpsSidenav menubuilder helper', () => {
-  it('runs methods without errors', () => {
+  it('runs opsLapsiLinkit & oppiaineLinkki without errors', () => {
     const res1 = opsLapsiLinkit([{
       tekstikappale: null,
       id: 1,
@@ -17,13 +17,40 @@ describe('OpsSidenav menubuilder helper', () => {
       }]
     }]);
     expect(res1.length).toBe(1);
+    expect(res1[0].item.type).toEqual('tekstikappale');
 
-    const res2 = oppimaaraModuuliLinkit({
+    const res2 = oppiaineLinkki('test', { id: null }, []);
+    expect(res2.item.type).toEqual('test');
+  });
+
+  it('runs oppimaaraModuuliLinkit & oppimaaraOpintojaksoLinkit without errors', () => {
+    const res1 = oppimaaraModuuliLinkit({
       id: 1,
       moduulit: [{
         id: 2,
       }],
     });
+    expect(res1.length).toBe(1);
+    expect(res1[0].item.type).toEqual('moduuli');
+
+    const res2 = oppimaaraOpintojaksoLinkit([
+      {
+        oppiaineet: [{
+          koodi: 'testiuri',
+        }]
+      }
+    ], {
+      koodi: {
+        uri: 'testiuri',
+      }
+    }
+    );
     expect(res2.length).toBe(1);
+    expect(res2[0].item.type).toEqual('opintojakso');
+  });
+
+  it('runs paikallinenOppiaineToMenu without errors', () => {
+    const res1 = paikallinenOppiaineToMenu({ id: 1 });
+    expect(res1.item.type).toEqual('oppiaine');
   });
 });
