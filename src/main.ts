96887c60963bc705343659c2c6adb8d9c0adcd50
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-03-18 16:16:58 +0200

    Kielilokalisointi fix

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,22 @@ import Vue from 'vue';
 import '@/config/styles';
 import { getRootConfig } from '@/mainvue';
 import { createLogger } from '@shared/utils/logger';
+import { Ulkopuoliset } from '@shared/api/ylops';
+import { Kielet } from '@shared/stores/kieli';
 
 const logger = createLogger('Main');
 
 Vue.config.productionTip = false;
 
+async function getLokalisoinnit() {
+  return (await Ulkopuoliset.getLokalisoinnit()).data as any;
+}
+
 async function main() {
   try {
     logger.info('Mounting #app');
     (new Vue(await getRootConfig())).$mount('#app');
+    await Kielet.load(await getLokalisoinnit());
   }
   catch (err) {
     logger.error('Top level error:" ', err);
