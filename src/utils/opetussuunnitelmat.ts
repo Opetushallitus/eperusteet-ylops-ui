2f76cb631d3a537d3b25512535cfa789a804c317
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-05-07 17:15:52 +0300

    EP-2258

--- src/utils/opetussuunnitelmat.ts
+++ src/utils/opetussuunnitelmat.ts
@@ -1,5 +1,6 @@
 import { OpetussuunnitelmaInfoDtoToteutusEnum } from '@shared/api/ylops';
 import * as _ from 'lodash';
+import { Kielet } from '@shared/stores/kieli';
 
 export function isOpsToteutusSupported(ops: any) {
   const { toteutus } = ops;
@@ -8,4 +9,21 @@ export function isOpsToteutusSupported(ops: any) {
     OpetussuunnitelmaInfoDtoToteutusEnum.YKSINKERTAINEN.toLowerCase(),
     OpetussuunnitelmaInfoDtoToteutusEnum.PERUSOPETUS.toLowerCase(),
   ], toteutus);
+}
+
+export function sortedOppiaineet(oppianeet) {
+  return _.chain(oppianeet)
+    .sortBy(oppiaine => Kielet.kaanna(oppiaine.oppiaine.nimi))
+    .sortBy(oppiaine => oppiaine.jnro)
+    .map('oppiaine')
+    .map(oppiaine => {
+      return {
+        ...oppiaine,
+        oppimaarat: _.chain(oppiaine?.oppimaarat)
+          .sortBy(oppimaara => Kielet.kaanna(oppimaara.nimi))
+          .sortBy(oppimaara => _.get(_.head(oppimaara.vuosiluokkakokonaisuudet), 'jnro'))
+          .value(),
+      };
+    })
+    .value();
 }
