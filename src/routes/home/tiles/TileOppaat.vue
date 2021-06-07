4a0bb2f92043ae3c73bcbad5da1a1d60d24f9c08
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-06-07 16:54:31 +0300

    EP-2831

---
+++ src/routes/home/tiles/TileOppaat.vue
@@ -0,0 +1,26 @@
+<template>
+  <EpHomeTile icon="ohjeet" :route="{ name: 'oppaat' }">
+    <template slot="header">
+      <span>{{ $t('tile-ohjeet-ja-materiaalit') }}</span>
+    </template>
+    <template slot="content">
+      <p>{{ $t('tile-ohjeet-ja-materiaalit-kuvaus') }}</p>
+    </template>
+  </EpHomeTile>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Provide } from 'vue-property-decorator';
+import EpHomeTile from '@shared/components/EpHomeTiles/EpHomeTile.vue';
+import EpSpinner from '@shared/components/EpSpinner/EpSpinner.vue';
+
+@Component({
+  components: {
+    EpHomeTile,
+    EpSpinner,
+  },
+})
+export default class TileOppaat extends Vue {
+
+}
+</script>
