8a5aa9318a2184de73a83651258f38a03a5cf7b4
Timo Paasila <timo.paasila@gofore.com>
2023-01-13 08:36:18 +0200

    Frontend-utils update

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,6 +3,11 @@ declare module '*.vue' {
   export default Vue;
 }
 
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
+
 declare module '*.svg' {
   const value: any;
   export = value;
