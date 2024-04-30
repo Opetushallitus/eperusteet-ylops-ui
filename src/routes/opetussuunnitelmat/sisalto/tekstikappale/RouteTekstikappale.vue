df27f7b6e583de56220cc9fa66b31c20e4b0e6f5
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-04-30 14:53:36 +0300

    bugfix/EP-4209

--- src/routes/opetussuunnitelmat/sisalto/tekstikappale/RouteTekstikappale.vue
+++ src/routes/opetussuunnitelmat/sisalto/tekstikappale/RouteTekstikappale.vue
@@ -173,7 +173,6 @@ export default class RouteTekstikappale extends Mixins(EpRoute, EpOpsComponent) 
       restore: this.restore,
     },
     copy: this.copy,
-    copyable: this.copyable,
     editable: this.editable,
   };
 
@@ -288,8 +287,6 @@ export default class RouteTekstikappale extends Mixins(EpRoute, EpOpsComponent) 
         this.breadcrumb('tekstikappale', teksti.tekstiKappale.nimi);
       }
 
-      this.kopioitava = result.tov.omistussuhde === 'lainattu';
-
       return result;
     }
   }
@@ -333,10 +330,6 @@ export default class RouteTekstikappale extends Mixins(EpRoute, EpOpsComponent) 
   async copy(kopioitava) {
     await this.store.kopioiTeksti(kopioitava.tov);
     this.$router.go(0);
-  }
-
-  async copyable() {
-    return this.kopioitava;
   }
 
   async editable() {
