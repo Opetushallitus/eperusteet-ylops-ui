67ef61aa0acdfdd6c767262856fd9a385c78bd42
Olli-Pekka Lehtokallio <53811784+oplekal@users.noreply.github.com>
2026-03-16 10:40:48 +0200

    EEP-4981 (#814)

--- src/stores/opetussuunnitelma.ts
+++ src/stores/opetussuunnitelma.ts
@@ -140,7 +140,7 @@ export class OpetussuunnitelmaStore {
       await this.updateValidation(),
       await this.fetchJulkaisut(),
       await this.updateOppiaineet(),
-      await this.updatePohjallaPuuttuviaTeksteja()
+      await this.updatePohjallaPuuttuviaTeksteja(),
     ]);
   }
 
